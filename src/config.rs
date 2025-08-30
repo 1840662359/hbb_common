3796286da4265e746a6b3c5adddbe9628e1c41f3
MoY <1840662359@qq.com>
2025-08-31 01:44:43 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["120.25.189.16"];
+pub const RS_PUB_KEY: &str = "Z0L0oV1YIUUZitQBg+zNbi6LcNhGH8WWx2JpLhFUYjg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
